3689ddf047f6038a54557446efbdeb3312e6dc42
Gaston <gaslif@gmail.com>
2022-09-25 18:30:04 -0300

    Add 2 ex to file_helper

--- file_helper.py
+++ file_helper.py
@@ -1,15 +1,20 @@
 from constants import mRNA_GENBANK_FILES
 
-input_folder = "inputs/"
-output_folder = "outputs/"
 
 def file_param_to_file_name(param):
     file_names = {
         "1A": mRNA_GENBANK_FILES.ISOFORM_A_PREPROTEIN.value,
         "1B": mRNA_GENBANK_FILES.ISOFORM_B.value,
+        "2": mRNA_GENBANK_FILES.FASTA_FILE.value
+    }
+
+    folder_names = {
+        "1A": "inputs/",
+        "1B": "inputs/",
+        "2": "outputs/",
     }
  
-    return input_folder + file_names.get(param, "")
+    return folder_names.get(param, "") + file_names.get(param, "")
 
 def generate_output_path(filename):
-    return output_folder + filename
+    return "outputs/" + filename
